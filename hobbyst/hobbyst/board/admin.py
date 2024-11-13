9c85c3f9625b2a192f812a61bc4bf6c485e6ea42
D0H00N <yjl9622@gmail.com>
2024-11-13 18:04:51 +0900

    Add files via upload

--- hobbyst/hobbyst/board/admin.py
+++ hobbyst/hobbyst/board/admin.py
@@ -1,25 +1,55 @@
 from django.contrib import admin
-from board.models import Board,BoardImage,Comment
-
-# Register your models here.
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
+from board.models import Post,PostImage, Comment
 import admin_thumbnails
 
-@admin_thumbnails.thumbnail("photo")
-class BoardImageInline(admin.TabularInline):
-    model = BoardImage
+class CommentInline(admin.TabularInline):
+    model = Comment
     extra = 1
 
- 
-@admin.register(Board)
-class BoardAdmin(admin.ModelAdmin):
-    list_display = ["id","content",]
+class InlineImangeWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer= None):
+        html = super().render(name, value, attrs, renderer)
+        if value and getattr(value, 'url', None) :
+            html = mark_safe(f'<img src="{value.url}" height = 150>') + html
+        return html
 
+# 교재 p.245
+@admin_thumbnails.thumbnail("photo")
+class PostImagaInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+    # formfield_overrides = {
+    #     models.ImageField: {
+    #         "widget": InlineImangeWidget,
+    #     }
+    # }
 
-@admin.register(BoardImage)
-class BoardImageAdmin(admin.ModelAdmin):
-    list_display = ["id","board","photo",]
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'content',
+    ]
+    inlines = [
+        CommentInline,
+        PostImagaInline,
+    ]
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'post',
+        'photo',
+    ]
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id","board","content",]
-
+    list_display = [
+        'id',
+        'post',
+        'content',
+    ]
